220789856a3cc1e681e8e7ba7c418fce39755db8
MSimone12 <matheusosimone@hotmail.com>
2018-07-27 15:50:52 -0300

    up

--- app.js
+++ app.js
@@ -10,25 +10,109 @@ module.exports = class App {
         var inputFinal = input.replace(/[^0-9]/g, "");
 
         if(inputFinal.length != 47){
-            return ({erro: "boleto não é valido"});
+            return ({sucesso: false, mensagem: "Boleto não é valido"});
         }
         else
         {
+            var verifica = this.VerificaDV(inputFinal);
+            console.log(verifica);
+            if (verifica) {
+                var valor = this.CalculaValor(inputFinal);
+                var vencimento = this.CalculaData(inputFinal);
+                var cod = this.CalculaCodBarras(inputFinal);
+                var inst = this.InstituicaoFinanceira(inputFinal);
 
-            var valor = this.CalculaValor(inputFinal);
-            var vencimento = this.CalculaData(inputFinal);
-            var cod = this.CalculaCodBarras(inputFinal);
-            var inst = this.InstituicaoFinanceira(inputFinal);
+                var json = {
+                    "instituicao": inst,
+                    "valor": valor,
+                    "vencimento": vencimento,
+                    "codigo": cod,
+                }
 
-            var json = {
-                "instituicao": inst,
-                "valor": valor,
-                "vencimento": vencimento,
-                "codigo": cod,
+                return ({sucesso: true, data: json});    
+            }else {
+                return ({sucesso: false, mensagem: "Digitos não são válidos"});
             }
-
-            return JSON.stringify(json);
+            
         }
+
+    }
+
+    VerificaDV(input) {
+        var fator1 = [2,1,2,1,2,1,2,1,2];
+        var fator2 = [1,2,1,2,1,2,1,2,1,2];
+        var fator3 = [1,2,1,2,1,2,1,2,1,2];
+
+        var campo1 = input.substring(0,9);
+        var campo2 = input.substring(10,20);
+        var campo3 = input.substring(21,31);
+
+        var campo11 = campo1.split('');
+        var campo21 = campo2.split('');
+        var campo31 = campo3.split('');
+
+        var soma1 = 0;
+        var soma2 = 0;
+        var soma3 = 0;
+
+        for (let i = 0; i < campo11.length; i++) {
+            var element = campo11[i];
+
+            if((element * fator1[i]) > 9){
+                var mult = (element * fator1[i]).toString();
+                var mt = mult.split('');
+                soma1 = soma1 + (parseInt(mt[0], '10') + parseInt(mt[1], '10'));                 
+            }else{
+                soma1 = soma1 + (parseInt(element, '10') * parseInt(fator1[i], '10'));
+            }   
+        }
+
+        for (let i = 0; i < campo21.length; i++) {
+            var element = campo21[i];
+
+            if((element * fator2[i]) > 9){
+                var mult = (element * fator2[i]).toString();
+                var mt = mult.split('');
+                soma2 = soma2 + (parseInt(mt[0], '10') + parseInt(mt[1], '10'));                 
+            }else{
+                soma2 = soma2 + (parseInt(element, '10') * parseInt(fator2[i], '10'));
+            }
+        }
+
+        for (let i = 0; i < campo31.length; i++) {
+            var element = campo31[i];
+
+            if((element * fator3[i]) > 9){
+                var mult = (element * fator3[i]).toString();
+                var mt = mult.split('');
+                soma3 = soma3 + (parseInt(mt[0], '10') + parseInt(mt[1], '10'));                 
+            }else{
+                soma3 = soma3 + (parseInt(element, '10') * parseInt(fator3[i], '10'));
+            }
+        }
+
+        var resto1 = soma1 % 10;
+        var resto2 = soma2 % 10;
+        var resto3 = soma3 % 10;
+
+        var dv1 = input.substring(9,10);
+        var dv2 = input.substring(20,21);
+        var dv3 = input.substring(31,32);
+
+        var dv11 = (Math.ceil((soma1 / 10))*10) - resto1;
+        var dv21 = (Math.ceil((soma2 / 10))*10) - resto2;
+        var dv31 = (Math.ceil((soma3 / 10))*10) - resto3;
+
+        dv11 = dv11.toString().substring(1,2);
+        dv21 = dv21.toString().substring(1,2);
+        dv31 = dv31.toString().substring(1,2);
+
+        if (dv1 == dv11 && dv2 == dv21 && dv3 == dv31) {
+            return true;
+        } else {
+            return false;
+        }
+
 
     }
 
--- server.js
+++ server.js
@@ -9,6 +9,12 @@ const port = 4000;
 server.use(bodyParser.urlencoded({ extended: true}));
 server.use(bodyParser.json());
 
+server.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 router.get('/', function(req, res){
     console.log(req);
     res.send("API para boleto EWALLY");
@@ -17,8 +23,6 @@ router.get('/', function(req, res){
 router.post("/api", function(req, res){
     var a = new App();
     var resposta = a.CalcularDados(req.body.boleto);
-
-    console.log(resposta);
 
     res.json(resposta);
 })
